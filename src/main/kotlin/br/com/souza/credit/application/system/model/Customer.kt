464e601b64b89dfb188cf07153298f8c8caa7c9f
Rafael Souza <36795291+rafaelhop@users.noreply.github.com>
2024-02-29 17:39:39 -0300

    Fix indentation

--- src/main/kotlin/br/com/souza/credit/application/system/model/Customer.kt
+++ src/main/kotlin/br/com/souza/credit/application/system/model/Customer.kt
@@ -6,19 +6,12 @@ import jakarta.persistence.*
 data class Customer(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
-
     @Column(nullable = false) var firstName: String = "",
-
     @Column(nullable = false) var lastName: String = "",
-
     @Column(nullable = false, unique = true) val cpf: String,
-
     @Column(nullable = false, unique = true) var email: String = "",
-
     @Column(nullable = false) var password: String = "",
-
     @Column(nullable = false) @Embedded  var address: Address = Address(),
-
     @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.REMOVE), mappedBy = "customer")
     var credits: List<Credit> = mutableListOf()
 )
